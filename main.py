247fdce50d02f4b98800f3b28be40baac44660a3
Imasandi <91633499+Imasandi@users.noreply.github.com>
2023-03-13 00:21:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# variables
+#different types of data that can be included inside variables
+"""3 types of data inside a variable
+1: string - plane text
+2: number - decimal, whole, without quotation marks
+3: boolean - true / false
+"""
+
+character_name = "Tom"
+character_age = "35"
+
+print("there once was a man named " + character_name+ ",")
+print("he was "+ character_age+ " years old.")
+
+# update the detailes in variables
+
+character_name = "Mice" # strings, plane text
+character_age = "23" # numbers
+is_male = False # boolean value -  True/ False
+print("he really liked his name "+ character_name+ ",")
+print("but didn't like being " +character_age+ ".")
+
+#create a variable for name and age. if you want to change the name and age of the man. if it is a long story you can't change it manually.
+# 1.name of the variable
+# 2.put a value inside the variable
+# 3.replace the character_name inside the story with the variable
+
+
